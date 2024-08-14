1ba12bc28061f36b7c680607ca19c82e5c097852
Aoneahsan <aoneahsan@gmail.com>
2024-08-14 12:15:50 +0500

    'updated

--- src/mpw/helpers/packagesHelpers/index.ts
+++ src/mpw/helpers/packagesHelpers/index.ts
@@ -1,16 +1,17 @@
-import { z } from 'zod';
-import type { ZodType } from 'zod';
+// import { z } from 'zod';
+// import type { ZodType } from 'zod';
 
-export function numericEnum<TValues extends readonly number[]>(
-	values: TValues
-) {
-	return z.number().superRefine((val, ctx) => {
-		if (!values.includes(val)) {
-			ctx.addIssue({
-				code: z.ZodIssueCode.invalid_enum_value,
-				options: [...values],
-				received: val,
-			});
-		}
-	}) as ZodType<TValues[number]>;
-}
+// export function numericEnum<TValues extends readonly number[]>(
+// 	values: TValues
+// ) {
+// 	return z.number().superRefine((val, ctx) => {
+// 		if (!values.includes(val)) {
+// 			ctx.addIssue({
+// 				code: z.ZodIssueCode.invalid_enum_value,
+// 				options: [...values],
+// 				received: val,
+// 			});
+// 		}
+// 	}) as ZodType<TValues[number]>;
+// }
+export { }
