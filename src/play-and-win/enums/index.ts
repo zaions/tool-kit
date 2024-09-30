c23d9b9efd40598a343252025879621b7b863719
Ahsan Mahmood <aoneahsan@gmail.com>
2024-09-30 23:57:30 +0500

    updated

--- src/play-and-win/enums/index.ts
+++ src/play-and-win/enums/index.ts
@@ -1,64 +1,67 @@
 export enum BalanceUpdateActionEnum {
-    add = 1,
-    subtract = 2,
+	add = 1,
+	subtract = 2,
 }
 export enum BalanceUpdateTypeEnum {
-    exact = 1,
-    percentage = 2,
+	exact = 1,
+	percentage = 2,
 }
 
 export enum PlayerBalanceUpdateRequestTypeEnum {
-    topup = 1,
-    withdraw = 2,
-    transferred = 3,
-    received = 4,
+	topup = 1,
+	withdraw = 2,
+	transferred = 3,
+	received = 4,
+	wonGameRoomPoolPrice = 5,
+	gameRoomFeeReturnedAfterReleased = 6,
+	lostGameRoom = 7,
 }
 
 export enum InAppNotificationSendToEnum {
-    allUsers = 1,
-    allAdmins = 2,
-    allEngagers = 3,
-    allPlayers = 4,
-    allEngagersAndPlayers = 5,
-    specificUsers = 6,
+	allUsers = 1,
+	allAdmins = 2,
+	allEngagers = 3,
+	allPlayers = 4,
+	allEngagersAndPlayers = 5,
+	specificUsers = 6,
 }
 
-
 export enum InAppNotificationStatusEnum {
-    sent = 1,
-    pending = 2,
-    draft = 3,
+	sent = 1,
+	pending = 2,
+	draft = 3,
 }
 
 export enum GameRoomStatusEnum {
-    waitingForPlayers = 1,
-    inProgress = 2,
-    waitingForResult = 3,
-    completed = 4,
-    waitingForEngager = 5, // once required players have entered the room and are waiting for the engager, to start the game
-    released = 6, // when the required players were not able to join the game room in time and engager released the game room
+	waitingForPlayers = 1,
+	inProgress = 2,
+	waitingForResult = 3,
+	completed = 4,
+	waitingForEngager = 5, // once required players have entered the room and are waiting for the engager, to start the game
+	released = 6, // when the required players were not able to join the game room in time and engager released the game room
 }
-
 
 export enum WithdrawRequestStatusEnum {
-    processing = 1,
-    completed = 2,
-    rejected = 3,
-    cancelled = 4,
+	processing = 1,
+	completed = 2,
+	rejected = 3,
+	cancelled = 4,
 }
 
-
 export enum TopupStatusEnum {
-    paymentVerificationRequired = 1,
-    waitingForReview = 2,
-    completed = 3,
-    rejected = 4,
-    cancelled = 5,
+	paymentVerificationRequired = 1,
+	waitingForReview = 2,
+	completed = 3,
+	rejected = 4,
+	cancelled = 5,
 }
 
 export enum TransactionTypeEnum {
-    topup = 1,
-    withdraw = 2,
-    transfer = 3,
-    received = 4,
+	topup = 1,
+	withdraw = 2,
+	transfer = 3,
+	received = 4,
+	wonGameRoomPoolPrice = 5,
+	gameRoomFeeReturnedAfterReleased = 6,
+	lostGameRoom = 7,
 }
