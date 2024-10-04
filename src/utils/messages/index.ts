7423d3e5e7929fd15ccaa0a1ecb089173c0ac964
Aoneahsan <aoneahsan@gmail.com>
2024-10-04 12:42:54 +0500

    main - updated and pushed

--- src/utils/messages/index.ts
+++ src/utils/messages/index.ts
@@ -2,19 +2,23 @@
  * @summary Messages from @zaions/tool-kit package
  */
 export const ztkMessages = {
-	general: {
-		success: 'Request Completed',
-		failed: 'Request failed',
-		created: 'item created',
-		updated: 'item updated',
-		deleted: 'item deleted',
-		notFound: 'item notFound',
-		invalidData: 'invalid data',
-		tooManyRequests: 'tooManyRequests',
-		unAuthenticated: 'unAuthenticated',
-		unAuthorized: 'unAuthorized',
-		serverError: 'serverError',
-		badRequest: 'badRequest',
-		itemExists: 'itemExists',
-	},
+  general: {
+    success: "Request Completed",
+    failed: "Request failed",
+    created: "item created",
+    updated: "item updated",
+    deleted: "item deleted",
+    notFound: "item notFound",
+    invalidData: "invalid data",
+    tooManyRequests: "tooManyRequests",
+    unAuthenticated: "unAuthenticated",
+    unAuthorized: "unAuthorized",
+    serverError: "serverError",
+    badRequest: "badRequest",
+    itemExists: "itemExists",
+  },
+  profile: {
+    added: "Profile data added successfully.",
+    updated: "Profile data updated successfully.",
+  },
 } as const;
