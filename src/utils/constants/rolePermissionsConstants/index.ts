dabc0fa18538f0eeec51caffb103bb819cb1b7f6
Ahsan Mahmood <aoneahsan@gmail.com>
2024-10-08 17:37:28 +0500

    permissions updated

--- src/utils/constants/rolePermissionsConstants/index.ts
+++ src/utils/constants/rolePermissionsConstants/index.ts
@@ -2,57 +2,61 @@ import { RoleEnum } from '@enums/rolePermissionsEnum';
 import { getPermissions } from '@utils/helpers/rolePermissionsHelpers';
 
 const permissionsToExcludeForAdmin = [
-	'role',
-	'permission',
-	'forcedelete',
-	'trashed',
-	'transaction',
-	'appsetting',
-	'inappnotification',
+  'role',
+  'permission',
+  'forcedelete',
+  'trashed',
+  'transaction',
+  'appsetting',
+  'inappnotification',
+  'restore',
+  'addcoin',
+  'removecoin',
+  'admincoin',
 ];
 const permissionsToExcludeForManager = [
-	...permissionsToExcludeForAdmin,
-	'user',
-	'delete',
-	'topuprequest',
-	'withdrawrequest',
-	'transfercoinsrequest',
+  ...permissionsToExcludeForAdmin,
+  'user',
+  'delete',
+  'topuprequest',
+  'withdrawrequest',
+  'transfercoinsrequest',
 ];
 const permissionsToExcludeForEditor = [
-	...permissionsToExcludeForManager,
-	'block',
+  ...permissionsToExcludeForManager,
+  'block',
 ];
 const permissionsToExcludeForCreator = [
-	...permissionsToExcludeForManager,
-	'edit',
-	'update',
-	'review',
+  ...permissionsToExcludeForManager,
+  'edit',
+  'update',
+  'review',
 ];
 const permissionsToExcludeForReviewer = [
-	...permissionsToExcludeForCreator.filter((el) => el !== 'review'),
-	'create',
-	'add',
-	'hide',
+  ...permissionsToExcludeForCreator.filter((el) => el !== 'review'),
+  'create',
+  'add',
+  'hide',
 ];
 const permissionsToExcludeForEngager = [
-	...permissionsToExcludeForManager,
-	'review',
-	'user',
-	'hide',
+  ...permissionsToExcludeForManager,
+  'review',
+  'user',
+  'hide',
 ];
 const permissionsToIncludeForEngager = ['createPrivateGameRoom'];
 
 export const rolePermissionsMap: { [key in RoleEnum]: () => number[] } = {
-	[RoleEnum.superAdmin]: () => getPermissions([]),
-	[RoleEnum.admin]: () => getPermissions(permissionsToExcludeForAdmin),
-	[RoleEnum.manager]: () => getPermissions(permissionsToExcludeForManager),
-	[RoleEnum.editor]: () => getPermissions(permissionsToExcludeForEditor),
-	[RoleEnum.creator]: () => getPermissions(permissionsToExcludeForCreator),
-	[RoleEnum.reviewer]: () => getPermissions(permissionsToExcludeForReviewer),
-	[RoleEnum.player]: () => [],
-	[RoleEnum.engager]: () =>
-		getPermissions(
-			permissionsToExcludeForEngager,
-			permissionsToIncludeForEngager
-		),
+  [RoleEnum.superAdmin]: () => getPermissions([]),
+  [RoleEnum.admin]: () => getPermissions(permissionsToExcludeForAdmin),
+  [RoleEnum.manager]: () => getPermissions(permissionsToExcludeForManager),
+  [RoleEnum.editor]: () => getPermissions(permissionsToExcludeForEditor),
+  [RoleEnum.creator]: () => getPermissions(permissionsToExcludeForCreator),
+  [RoleEnum.reviewer]: () => getPermissions(permissionsToExcludeForReviewer),
+  [RoleEnum.player]: () => [],
+  [RoleEnum.engager]: () =>
+    getPermissions(
+      permissionsToExcludeForEngager,
+      permissionsToIncludeForEngager
+    ),
 };
