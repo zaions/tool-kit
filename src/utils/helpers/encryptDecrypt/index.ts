a281f2f87f575d8fa720be35d5cdd317ba2a471c
Ahsan Mahmood <aoneahsan@gmail.com>
2024-11-16 19:31:01 +0500

    cryptojs import updated from named to default

--- src/utils/helpers/encryptDecrypt/index.ts
+++ src/utils/helpers/encryptDecrypt/index.ts
@@ -1,19 +1,21 @@
-import { AES, enc } from 'crypto-js';
+import cryptoJs from 'crypto-js';
 import { getCryptoSecret } from 'src/privateModule';
 
 export const encryptData = (val: unknown): string | null => {
-	const _cryptoSecret = getCryptoSecret();
-	try {
-		return AES.encrypt(JSON.stringify(val), _cryptoSecret).toString();
-	} catch (error) {
-		return null;
-	}
+  const _cryptoSecret = getCryptoSecret();
+  try {
+    return cryptoJs.AES.encrypt(JSON.stringify(val), _cryptoSecret).toString();
+  } catch (error) {
+    return null;
+  }
 };
 export const decryptData = <T>(val: string): T | null => {
-	const _cryptoSecret = getCryptoSecret();
-	try {
-		return JSON.parse(AES.decrypt(val, _cryptoSecret).toString(enc.Utf8)) as T;
-	} catch (error) {
-		return null;
-	}
+  const _cryptoSecret = getCryptoSecret();
+  try {
+    return JSON.parse(
+      cryptoJs.AES.decrypt(val, _cryptoSecret).toString(cryptoJs.enc.Utf8)
+    ) as T;
+  } catch (error) {
+    return null;
+  }
 };
