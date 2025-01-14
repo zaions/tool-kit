1813e90416ea2e1927d3a775b83c1f50d28f5f35
Ahsan Mahmood <aoneahsan@gmail.com>
2025-01-15 02:47:11 +0500

    code logic updated

--- src/require-package/crypto-js/index.ts
+++ src/require-package/crypto-js/index.ts
@@ -1,10 +1,10 @@
-import { AES, enc } from 'crypto-js';
+import cryptoJs from 'crypto-js';
 import { getCryptoSecret } from 'src/privateModule';
 
 export const encryptData = (val: unknown): string | null => {
   const _cryptoSecret = getCryptoSecret();
   try {
-    return AES.encrypt(JSON.stringify(val), _cryptoSecret).toString();
+    return cryptoJs.AES.encrypt(JSON.stringify(val), _cryptoSecret).toString();
   } catch (error) {
     return null;
   }
@@ -12,7 +12,9 @@ export const encryptData = (val: unknown): string | null => {
 export const decryptData = <T>(val: string): T | null => {
   const _cryptoSecret = getCryptoSecret();
   try {
-    return JSON.parse(AES.decrypt(val, _cryptoSecret).toString(enc.Utf8)) as T;
+    return JSON.parse(
+      cryptoJs.AES.decrypt(val, _cryptoSecret).toString(cryptoJs.enc.Utf8)
+    ) as T;
   } catch (error) {
     return null;
   }
@@ -22,8 +24,13 @@ export const encryptWithSecretKey = (
   data: unknown,
   secretKey: string
 ): string => {
-  const encJson = AES.encrypt(JSON.stringify(data), secretKey).toString();
-  const encData = enc.Base64.stringify(enc.Utf8.parse(encJson));
+  const encJson = cryptoJs.AES.encrypt(
+    JSON.stringify(data),
+    secretKey
+  ).toString();
+  const encData = cryptoJs.enc.Base64.stringify(
+    cryptoJs.enc.Utf8.parse(encJson)
+  );
 
   return encData;
 };
@@ -32,8 +39,12 @@ export const decryptWithSecretKey = <T = any>(
   encryptedData: string,
   secretKey: string
 ): T => {
-  const decData = enc.Base64.parse(encryptedData).toString(enc.Utf8);
-  const bytes = AES.decrypt(decData, secretKey).toString(enc.Utf8);
+  const decData = cryptoJs.enc.Base64.parse(encryptedData).toString(
+    cryptoJs.enc.Utf8
+  );
+  const bytes = cryptoJs.AES.decrypt(decData, secretKey).toString(
+    cryptoJs.enc.Utf8
+  );
 
   return JSON.parse(bytes) as T;
 };
@@ -42,9 +53,9 @@ export const aesDecrypt = <T = any>(
   data: string,
   secretKey: string
 ): T | null => {
-  const bytes = AES.decrypt(data, secretKey);
+  const bytes = cryptoJs.AES.decrypt(data, secretKey);
   try {
-    return JSON.parse(bytes.toString(enc.Utf8)) as T;
+    return JSON.parse(bytes.toString(cryptoJs.enc.Utf8)) as T;
   } catch (error) {
     return null;
   }
